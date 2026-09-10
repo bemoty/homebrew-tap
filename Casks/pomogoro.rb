058035471df66f0d2c3436ef08a3e126221e33e3
Joshua Winkler <josh@bemoty.dev>
2026-09-10 11:35:54 +0000

    pomogoro v0.1.2

--- Casks/pomogoro.rb
+++ Casks/pomogoro.rb
@@ -1,14 +1,14 @@
 cask "pomogoro" do
-  version "0.1.1"
+  version "0.1.2"
 
   on_arm do
-    url "https://github.com/bemoty/pomogoro/releases/download/v0.1.1/pomogoro-arm64.dmg"
-    sha256 "192b4164313aba56dedb55324038ca09994de575f305faf8fbff71c7f36ebc77"
+    url "https://github.com/bemoty/pomogoro/releases/download/v0.1.2/pomogoro-arm64.dmg"
+    sha256 "1058bc2fa9f93db91cc84935c2a8d9b844feafc440104c1c9b31c4a7a4a9153f"
   end
 
   on_intel do
-    url "https://github.com/bemoty/pomogoro/releases/download/v0.1.1/pomogoro-amd64.dmg"
-    sha256 "cb42d8e539f496b6e10e40bd002f64d606567f111de944f3b9e199ccf24ecdde"
+    url "https://github.com/bemoty/pomogoro/releases/download/v0.1.2/pomogoro-amd64.dmg"
+    sha256 "01991dc6e9152116e4d8e9055ff313ce0399d997f910c9eaf95a9cda039a4c14"
   end
 
   name "pomogoro"
